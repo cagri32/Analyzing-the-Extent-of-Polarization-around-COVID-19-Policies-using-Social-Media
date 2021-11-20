5cba6ce22b807f304449732e4a0ea0f3e585579a
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2021-11-19 19:37:10 -0500

    Add main.py that reads data and creates the graph

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import pandas as pd
+from collections import Counter
+import networkx as nx
 
+def main():
+  graph = nx.DiGraph()
+
+  columns = ['user_id', 'retweeted_user_id']
+  df = pd.read_csv('hydrated_tweets.csv',usecols=columns)
+
+  for row in df.itertuples(index=False):
+    if row.retweeted_user_id != -1:
+      graph.add_edge(row.user_id,str(row.retweeted_user_id))
+
+  print(graph.edges)
+  print(graph)
+
+if __name__ == "__main__":
+    main()
