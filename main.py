2856dccd9ee7dfe59a4d2aba20290e22554822fc
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2021-11-23 17:23:27 -0500

    Export the graph to a file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from collections import Counter
 import networkx as nx
+import pickle
+
 
 def main():
   graph = nx.DiGraph()
@@ -12,8 +14,15 @@ def main():
     if row.retweeted_user_id != -1:
       graph.add_edge(row.user_id,str(row.retweeted_user_id))
 
-  print(graph.edges)
+  #print(graph.edges)
   print(graph)
+
+  with open("graph.txt", 'wb') as f:
+    pickle.dump(graph, f)
+
+  #pickle.dump(graph, open('graph.txt', 'w'))
+
+  return graph
 
 if __name__ == "__main__":
     main()
